e905f0ddcc9e7f9f690a2b86dfe115156712f9b8
anto <antobarbero94@gmail.com>
2018-05-06 19:17:37 -0300

    coding style

--- exercise_2.py
+++ exercise_2.py
@@ -1,34 +1,35 @@
 
 def anagrams(word):
-	"""
-	Use the file WORD.LST and returns anagrams
-	of the word given in parameter word
-	"""
-	words = [
-		w.rstrip() for w in open('WORD.lst')
-		if len(w.rstrip()) == len(word) and w.rstrip() != word
-	]
-	anagram_list = []
+    """
+    Use the file WORD.LST and returns anagrams
+    of the word given in parameter word
+    """
+    words = [
+        w.rstrip() for w in open('WORD.lst')
+        if len(w.rstrip()) == len(word) and w.rstrip() != word
+    ]
 
-	for w in words:
-		tmp_word = list(word)
-		is_anagram = True
-		for letter in w:
-			if letter not in tmp_word:
-				is_anagram = False
-				break
-			else:
-				tmp_word.remove(letter)
+    anagram_list = []
 
-		if is_anagram:
-			anagram_list.append(w)
+    for w in words:
+        tmp_word = list(word)
+        is_anagram = True
+        for letter in w:
+            if letter not in tmp_word:
+                is_anagram = False
+                break
+            else:
+                tmp_word.remove(letter)
 
-	return anagram_list
+        if is_anagram:
+            anagram_list.append(w)
+
+    return anagram_list
 
 
 if __name__ == "__main__":
-	print(anagrams("train"))
-	print('--')
-	print(anagrams("drive"))
-	print('--')
-	print(anagrams("python"))
+    print(anagrams("train"))
+    print('--')
+    print(anagrams("drive"))
+    print('--')
+    print(anagrams("python"))
